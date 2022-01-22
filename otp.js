834a0d2304881131b282291d1d620c047c0c31a0
codewithritik <97462674+codewithritik@users.noreply.github.com>
2022-01-22 15:03:18 +0530

    Delete otp.js

--- otp.js
+++
@@ -1,9 +0,0 @@
-otpinput.addEventListener("keyup",myotp)
-
-function myotp(){
-    var input = document.getElementById("otpinput").value
-    if(input=="12345"){
-        window.location.href = "loginpage.html";
-    }
-
-}
